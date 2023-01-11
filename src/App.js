d29c3d07550e6521e7a4fc032713d997363f6481
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-01-11 19:19:35 +0100

    add search page with lazy query fetch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import CharactersList from './pages/CharactersList';
 import { Route, Routes } from 'react-router-dom';
 import Character from './pages/Character';
+import Search from './pages/Search';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route strict exact path='/' element= {<CharactersList />} />
+        <Route strict exact path='/search' element= {<Search />} />
         <Route strict exact path='/:id' element= {<Character />} />
       </Routes>
     </div>
